04301d8c363d9129713d8a85c030f9cd7883046b
trahzi <koddosijustmadethis@gmail.com>
2024-08-28 23:02:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById("myButton").addEventListener("click", function() {
-    window.location.href = "https://discord.com/oauth2/authorize?client_id=1275907615875924068";
+    window.open("https://discord.com/oauth2/authorize?client_id=1275907615875924068", "_blank");
 });
