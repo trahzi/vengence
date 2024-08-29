e93283536ae97fd812e70b2cc7cadeb90ef9d7f2
trahzi <koddosijustmadethis@gmail.com>
2024-08-28 23:15:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-document.getElementById("myButton").addEventListener("click", function() {
+document.getElementById("addBotButton").addEventListener("click", function() {
     window.open("https://discord.com/oauth2/authorize?client_id=1275907615875924068", "_blank");
+});
+
+document.getElementById("supportButton").addEventListener("click", function() {
+    window.open("https://discord.gg/dY4PQkfc2z", "_blank");
 });
